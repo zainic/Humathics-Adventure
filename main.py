4a9c25e9e4d4a870b21d056f38e027f020c76afc
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-07-13 22:31:28 +0700

    Call the main menu function phase

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import numpy as np
 import cv2
 import os, sys
 
-from object import *
+from pynput import keyboard
+
+from data.function.object import *
+from data.function.function import *
+from data.function.main_menu import *
+
+global pressed_keys
+
+pressed_keys = set()
+
+def on_pressed(key):
+    pressed_keys.add(key)
+
+def on_released(key):
+    pressed_keys.remove(key)
 
 def main():
-    pass
+    listener = keyboard.Listener(on_press=on_pressed, on_release=on_released)
+    listener.start()
+    main_menu(pressed_keys)
+    listener.stop()
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
