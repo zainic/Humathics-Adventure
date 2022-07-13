822fe26cb3bb971b89a54598941714be004be4ba
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-07-13 23:37:27 +0700

    Added code for showing the button

--- data/function/main_menu.py
+++ data/function/main_menu.py
@@ -2,16 +2,24 @@ import numpy as np
 import cv2
 import os, sys
 
+from pynput import keyboard
+
 from .object import *
 from .function import *
 
-def main_menu(pressed_keys):
+def main_menu():
     """
     Main menu phase
     """
     background = Background(os.path.join("data", "texture", "main_menu", "background.png"))
+    play_button = Button(os.path.join("data", "texture", "main_menu", "play_button_selected.png"), os.path.join("data", "texture", "main_menu", "play_button.png"))
+    exit_button = Button(os.path.join("data", "texture", "main_menu", "exit_button_selected.png"), os.path.join("data", "texture", "main_menu", "exit_button.png"))
+    
+    listener = keyboard.Listener(on_press=on_pressed, on_release=on_released)
+    listener.start()
+    
     while True:
-        frame = create_main_menu_frame(background)
+        frame = create_main_menu_frame(background, layers = [play_button, exit_button])
         
         cv2.imshow("Humathics Adventure", frame)
         
@@ -19,6 +27,10 @@ def main_menu(pressed_keys):
         
         background.move_background(LEFT, 2)
         
+        print(pressed_keys)
+        
         if get_exit_status(key):
             break
+    
+    listener.stop()
     
