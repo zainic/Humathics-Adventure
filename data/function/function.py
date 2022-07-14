72ed93960f37e13130fed240607d1be4652cef9c
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2022-07-14 18:33:59 +0700

    Added level selection page

--- data/function/function.py
+++ data/function/function.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os, sys
+import time
 
 from pynput import keyboard
 
@@ -18,7 +19,7 @@ def on_released(key):
 
 def create_main_menu_frame(background ,layers = []):
     """
-    Create frame from some condition
+    Create frame from some condition in main menu frame
 
     Args:
         background (class, optional): background. Defaults to Background().
@@ -34,18 +35,103 @@ def create_main_menu_frame(background ,layers = []):
             button_counter += 1
             if layer.select == False:
                 texture_unselected = np.copy(layer.unselected)
-                position = (4 * frame.shape[0] // 5 - texture_unselected.shape[0] // 2 - 50, button_counter * frame.shape[1] // 3 - texture_unselected.shape[1] // 2)
+                position = (4 * frame.shape[0] // 5 - texture_unselected.shape[0] // 2, button_counter * frame.shape[1] // 3 - texture_unselected.shape[1] // 2)
                 part_button = np.copy(frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]])
                 part_button[np.where((texture_unselected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
                 frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]] = cv2.addWeighted(part_button, 1, texture_unselected, 1, 0)
             else:
                 texture_selected = np.copy(layer.selected)
-                position = (4 * frame.shape[0] // 5 - texture_selected.shape[0] // 2 - 50, button_counter * frame.shape[1] // 3 - texture_selected.shape[1] // 2)
+                position = (4 * frame.shape[0] // 5 - texture_selected.shape[0] // 2, button_counter * frame.shape[1] // 3 - texture_selected.shape[1] // 2)
                 part_button = np.copy(frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]])
                 part_button[np.where((texture_selected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
                 frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]] = cv2.addWeighted(part_button, 1, texture_selected, 1, 0)
                 
     return frame
+
+def create_level_selection_frame(background ,layers = []):
+    """
+    Create frame from some condition in level selection page
+
+    Args:
+        background (class, optional): background. Defaults to Background().
+        layers (list, optional): layers that overlay the background. Defaults to [].
+    """
+    
+    frame = np.copy(background.background)
+    button_counter = 0
+    
+    # Show layers
+    for layer in layers:
+        if layer.__name__ == "Button":
+            if button_counter < 15:
+                if layer.select == False:
+                    texture_unselected = np.copy(layer.unselected)
+                    position = (((button_counter//5) + 2) * frame.shape[0] // 6 - texture_unselected.shape[0] // 2 - 50, ((button_counter % 5) + 2) * frame.shape[1] // 8 - texture_unselected.shape[1] // 2)
+                    part_button = np.copy(frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]])
+                    part_button[np.where((texture_unselected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+                    frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]] = cv2.addWeighted(part_button, 1, texture_unselected, 1, 0)
+                else:
+                    texture_selected = np.copy(layer.selected)
+                    position = (((button_counter//5) + 2) * frame.shape[0] // 6 - texture_selected.shape[0] // 2 - 50, ((button_counter % 5) + 2) * frame.shape[1] // 8 - texture_selected.shape[1] // 2)
+                    part_button = np.copy(frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]])
+                    part_button[np.where((texture_selected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+                    frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]] = cv2.addWeighted(part_button, 1, texture_selected, 1, 0)
+            else:
+                if layer.select == False:
+                    texture_unselected = np.copy(layer.unselected)
+                    position = (6 * frame.shape[0] // 7 - texture_unselected.shape[0] // 2, frame.shape[1] // 4 - texture_unselected.shape[1] // 2)
+                    part_button = np.copy(frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]])
+                    part_button[np.where((texture_unselected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+                    frame[position[0] : position[0] + texture_unselected.shape[0], position[1] : position[1] + texture_unselected.shape[1]] = cv2.addWeighted(part_button, 1, texture_unselected, 1, 0)
+                else:
+                    texture_selected = np.copy(layer.selected)
+                    position = (6 * frame.shape[0] // 7 - texture_selected.shape[0] // 2, frame.shape[1] // 4 - texture_selected.shape[1] // 2)
+                    part_button = np.copy(frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]])
+                    part_button[np.where((texture_selected != [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+                    frame[position[0] : position[0] + texture_selected.shape[0], position[1] : position[1] + texture_selected.shape[1]] = cv2.addWeighted(part_button, 1, texture_selected, 1, 0)
+            button_counter += 1
+                    
+    return frame
+
+def show_fps(frame, st, ed):
+    """
+    Show the frame rate (debugging mode)
+
+    Args:
+        frame (array): frame that will be showed
+        st (float): start time process
+        et (float): end time process
+    """
+    if ed - st <= 1/60:
+        time.sleep(1/60 - (ed - st))
+        fps = 60
+    else:
+        fps = round(1/(ed - st))
+    cv2.putText(frame, "fps : " + str(fps), (10, DEFAULT_WINDOW_HEIGHT - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+
+def get_direction_from_keys(keys):
+    """
+    function that return direction for ship from key pressed
+
+    Args:
+        keys (set): keys that got from Listener
+
+    Returns:
+        array: direction 
+    """
+    
+    direction = np.copy(NO_MOVE)
+    
+    if keyboard.Key.up in keys:
+        direction += UP
+    if keyboard.Key.down in keys:
+        direction += DOWN
+    if keyboard.Key.left in keys:
+        direction += LEFT
+    if keyboard.Key.right in keys:
+        direction += RIGHT
+        
+    return direction
 
 def get_enter_status(pressed_keys):
     """
--- data/function/main_menu.py
+++ data/function/main_menu.py
@@ -19,6 +19,12 @@ def main_menu():
     
     play_button.toggle_button()
     
+    levels_button = {}
+    for i in range(15):
+        levels_button[((i//5) + 1, (i % 5) + 1)] = Button(os.path.join("data", "texture", "main_menu", "level_"+ str(i+1) +"_selected.png"), os.path.join("data", "texture", "main_menu", "level_"+ str(i+1) +".png"))
+    
+    back_button = Button(os.path.join("data", "texture", "main_menu", "back_to_main_menu_selected.png"), os.path.join("data", "texture", "main_menu", "back_to_main_menu.png"))
+    
     switch_button = mixer.Sound(os.path.join("data","sound","switch_button.wav"))
     select_button = mixer.Sound(os.path.join("data","sound","select_button.wav"))
     
@@ -31,12 +37,7 @@ def main_menu():
         frame = create_main_menu_frame(background, layers = [play_button, exit_button])
         
         ed = time.time()
-        if ed - st <= 1/48:
-            time.sleep(1/48 - (ed - st))
-            fps = 48
-        else:
-            fps = round(1/(ed - st))
-        cv2.putText(frame, "fps : " + str(fps), (10, DEFAULT_WINDOW_HEIGHT - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        show_fps(frame, st, ed)
         st = time.time()
         
         cv2.imshow("Humathics Adventure", frame)
@@ -57,11 +58,62 @@ def main_menu():
         if get_enter_status(pressed_keys):
             mixer.Sound.play(select_button)
             if play_button.select == True:
-                return 'level_selection'
+                stat = 'level_selection'
+                break
             elif exit_button.select == True:
-                return 'exit'
+                ed = time.time()
+                stat = 'exit'
     
-    time.sleep(1)
+    current_pos = np.array([1,1])
+    levels_button[(current_pos[0], current_pos[1])].toggle_button()
+    delay_move = 5
+    time.sleep(0.5)
+    
+    while stat == 'level_selection':
+        
+        frame = create_level_selection_frame(background, layers = list(levels_button.values()) + [back_button])
+        
+        ed = time.time()
+        show_fps(frame, st, ed)
+        st = time.time()
+        
+        cv2.imshow("Humathics Adventure", frame)
+        
+        key = cv2.waitKey(1) & 0xff
+        
+        background.move_background(LEFT, 2)
+        
+        direction = get_direction_from_keys(pressed_keys)
+        
+        if delay_move <= 0 and 1 <= current_pos[0] + direction[0] <= 3 and 1 <= current_pos[1] + direction[1] <= 5 and not (direction == NO_MOVE).all():
+            if back_button.select == True:
+                back_button.toggle_button()
+            else:
+                before_pos = np.copy(current_pos)
+                levels_button[(before_pos[0],before_pos[1])].toggle_button()
+            current_pos += direction
+            levels_button[(current_pos[0],current_pos[1])].toggle_button()
+            delay_move = 5
+            mixer.Sound.play(switch_button)
+        elif delay_move <= 0 and current_pos[0] + direction[0] == 4 and current_pos[1] + direction[1] == current_pos[1] and not (direction == NO_MOVE).all():
+            before_pos = np.copy(current_pos)
+            levels_button[(before_pos[0],before_pos[1])].toggle_button()
+            current_pos += direction
+            back_button.toggle_button()
+            delay_move = 5
+            mixer.Sound.play(switch_button)
+        else:
+            delay_move -= 1
+        
+        if get_enter_status(pressed_keys):
+            mixer.Sound.play(select_button)
+            if back_button.select:
+                stat = 'main_menu'
+            else:
+                stat = 'exit'
+            break
+        
     ed = time.time()
     listener.stop()
+    return stat
     
